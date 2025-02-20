3f8dd614b416e84432c07a6828b8471c4300ae8d
Ruben Eland <rubeneland@hotmail.com>
2025-02-20 09:59:58 +0100

    small update

--- cost_functions.py
+++ cost_functions.py
@@ -21,14 +21,13 @@ def transformer_overload_usrpenalty_cost(env, total_costs, user_satisfaction_lis
     # invalid_actions = 0
     # for cs in env.charging_stations:
     #     invalid_actions += cs.invalid_action_punishment
-    # cost += invalid_actions
+    # cost += invalid_actions * 0.01
 
         # if score < 0.6:
         #      cost += 45
 
     # # For every charging station connected to the transformer
     # for cs in env.charging_stations:
-
     #         # For every EV connected to the charging station
     #         for EV in cs.evs_connected:
     #             # If there is an EV connected
@@ -36,7 +35,7 @@ def transformer_overload_usrpenalty_cost(env, total_costs, user_satisfaction_lis
     #                 # Add cost for discharging action when SOC below min V2G threshold: 40%
     #                 soc = EV.get_soc()
     #                 if soc <= 0.4 and EV.actual_current < 0:
-    #                     cost += (0.4 - soc) * 10
+    #                     cost += (0.4 - soc) * 5
 
 
     # cost for charging action when SOC at maximum
@@ -60,7 +59,7 @@ def ProfitMax_TrPenalty_UserIncentives_safety(env, total_costs, user_satisfactio
     #             if EV is not None:
     #                 # Add reward for discharging action when EV = full and still a long time until departure to stimulate V2G
     #                 soc = EV.get_soc()
-    #                 if soc >= 0.5 and EV.actual_current < 0 and (EV.time_of_departure - env.current_step) > 20:
-    #                     reward += 0.01
+    #                 if soc > 0.45 and EV.actual_current < 0 and (EV.time_of_departure - env.current_step) > 24:
+    #                     reward += (soc - 0.45) * EV.actual_current * 0.05
         
     return reward
--- example.sh
+++ example.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name="exp1"
 #SBATCH --partition=compute
-#SBATCH --time=16:00:00
+#SBATCH --time=10:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=16G
+#SBATCH --mem-per-cpu=12G
 #SBATCH --account=education-eemcs-msc-ee
 
 module load 2024r1 openmpi miniconda3 py-pip
@@ -15,4 +15,4 @@ source "$(conda info --base)/etc/profile.d/conda.sh"
 
 conda activate EV2Gym_srl
 
-srun python train_safe_RL.py --train cvpo --cost_limit 1 --epoch 900 --train_num 4 --test_num 50
+srun python train_safe_RL.py --train cvpo --cost_limit 2 --epoch 500 --train_num 4 --test_num 50
